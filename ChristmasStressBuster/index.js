b8ecf3eaf9d18d2ab1c44469dc7ee8cc5d3f70ce
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-02 02:43:33 +0000

    fix: move time inside countdown fn

--- ChristmasStressBuster/index.js
+++ ChristmasStressBuster/index.js
@@ -7,10 +7,9 @@ let timeout;
 // - Write a function to start the countdown timer on the button click and display it in the DOM. Stop the timer when it reaches 0.
 btn.addEventListener('click', countdown)
 
-let time = parseFloat(timer.innerText) * 60;
-
 function countdown(){
     btn.setAttribute('disabled', true) // can't click it again
+    let time = parseFloat(timer.innerText) * 60;
 
     let interval = setInterval(()=>{
         let minutes = Math.floor(time / 60);
